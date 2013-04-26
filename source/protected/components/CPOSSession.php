c88a9d3b1378f97aa4422ae20766c8539492b9ab
snowfox0210 <lecaotri.tdt@gmail.com>
2013-04-26 09:10:27 +0700

    session

---
+++ source/protected/components/CPOSSession.php
@@ -0,0 +1,58 @@
+<?php
+class CPOSSession{
+    private $_session;
+    private $_array;
+    private $_label;
+    public function __construct($label='session'){
+        $this->_label = $label;
+        $this->_array = array();
+        $this->_session = new CHttpSession;
+        $this->update();
+    }
+    public function set($key,$value){
+        $count = count($key);
+        if(is_array($key)==true && $count>0){
+            if($count==2){
+               $this->_array[$key[0]][$key[1]] = $value;
+            }
+            else{
+                $this->_array[$key[0]] = $value;
+            }   
+        }
+        else{
+            return null;
+        }
+        $this->update();
+    }
+    public function get($key=null){
+        if(is_null($key)){
+            return $this->_array;
+        }
+        $count = count($key);
+        if(is_array($key)==true && $count>0){
+            if($count==2){
+               return $this->_array[$key[0]][$key[1]];
+            }
+            else{
+                return $this->_array[$key[0]];
+            }   
+            
+        }
+        else{
+            return null;
+        }
+    }
+    public function clear(){
+        $this->_array = array();
+        $this->update();
+    }
+    private function update(){
+        if(!isset($this->_session[$this->_label])){
+            $this->_session->add($this->_label,$this->_array);   
+        }
+        else{
+            $this->_session[$this->_label] = $this->_array;
+        }
+    }
+}
+?>
