78a497b23c379e9e3b062ec3ec55cdfe1ac44307
= <=>
2013-06-09 02:52:04 +0700

    add contact page

--- source/protected/models/ChiTietHoaDonBan.php
+++ source/protected/models/ChiTietHoaDonBan.php
@@ -4,59 +4,64 @@ Yii::import('application.models._base.BaseChiTietHoaDonBan');
 
 class ChiTietHoaDonBan extends BaseChiTietHoaDonBan
 {
-	public static function model($className=__CLASS__) {
-		return parent::model($className);
-	}
-    
-    public function relations() {
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    public function relations()
+    {
         return array(
-            'hoaDonBanHang'=>array(self::BELONGS_TO,'hoaDonBanHang','hoa_don_ban_id'),
-            'sanPham'=>array(self::BELONGS_TO,'SanPham','san_pham_id'),
+            'hoaDonBanHang' => array(self::BELONGS_TO, 'HoaDonBanHang', 'hoa_don_ban_id'),
+            'sanPham' => array(self::BELONGS_TO, 'SanPham', 'san_pham_id'),
         );
     }
 
 
-    public function them($params) {
+    public function them($params)
+    {
         // kiem tra du lieu con bi trung hay chua
 
-        if(!$this->kiemTraTonTai($params)) {
+        if (!$this->kiemTraTonTai($params)) {
             //neu khoa chua ton tai
             $this->setAttributes($params);
-                            if ($this->save())
-                        return 'ok';
+            if ($this->save())
+                return 'ok';
             else
                 return 'fail';
         } else
-                return 'dup-error';
+            return 'dup-error';
     }
 
-    public function capNhat($params) {
+    public function capNhat($params)
+    {
         // kiem tra du lieu con bi trung hay chua
-                if(!$this->kiemTraTonTai($params)) {
+        if (!$this->kiemTraTonTai($params)) {
             $this->setAttributes($params);
-                            if ($this->save())
-                                return 'ok';
-                else
-                    return 'fail';
+            if ($this->save())
+                return 'ok';
+            else
+                return 'fail';
         } else {
 
-        // so sanh ma cu == ma moi
-        if($this->soKhopMa($params)) {
-            $this->setAttributes($params);
-                            if ($this->save())
-                                return 'ok';
+            // so sanh ma cu == ma moi
+            if ($this->soKhopMa($params)) {
+                $this->setAttributes($params);
+                if ($this->save())
+                    return 'ok';
                 else
                     return 'fail';
-        } else
+            } else
                 return 'dup-error';
 
         }
     }
 
-    public function xoa() {
+    public function xoa()
+    {
         $relation = $this->kiemTraQuanHe($this->id);
-        if(!$relation) {
-            if($this->delete())
+        if (!$relation) {
+            if ($this->delete())
                 return 'ok';
             else
                 return 'fail';
@@ -66,23 +71,24 @@ class ChiTietHoaDonBan extends BaseChiTietHoaDonBan
     }
 
 
-    public function xuatFileExcel() {
+    public function xuatFileExcel()
+    {
         $criteria = new CDbCriteria;
 
-                                $criteria->compare('san_pham_id', $this->san_pham_id);
-                                $criteria->compare('hoa_don_ban_id', $this->hoa_don_ban_id);
-                                $criteria->compare('so_luong', $this->so_luong);
-                                $criteria->compare('don_gia', $this->don_gia);
-                                $criteria->compare('san_pham_tang_id', $this->san_pham_tang_id);
-        
-       /* $event = new CPOSSessionEvent();
-        $event->currentSession = Yii::app()->session['ChiTietHoaDonBan'];
-        $this->onAfterExport($event);*/
+        $criteria->compare('san_pham_id', $this->san_pham_id);
+        $criteria->compare('hoa_don_ban_id', $this->hoa_don_ban_id);
+        $criteria->compare('so_luong', $this->so_luong);
+        $criteria->compare('don_gia', $this->don_gia);
+        $criteria->compare('san_pham_tang_id', $this->san_pham_tang_id);
+
+        /* $event = new CPOSSessionEvent();
+         $event->currentSession = Yii::app()->session['ChiTietHoaDonBan'];
+         $this->onAfterExport($event);*/
 
         return new CActiveDataProvider($this, array(
-        'criteria' => $criteria,
+            'criteria' => $criteria,
         ));
-        }
+    }
 
 
 }
